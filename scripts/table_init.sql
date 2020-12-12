4e6b8fbef15dd6f8af93cf19d6fb56b2851162c8
Aleksandr Parfenov <asp437@gmail.com>
2020-12-12 15:40:46 +0300

    Draft of SQL schema

---
+++ scripts/table_init.sql
@@ -0,0 +1,31 @@
+DROP TABLE test_places;
+DROP TABLE med_organisations;
+DROP TABLE cities;
+
+CREATE TABLE med_organisations (
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(256) NOT NULL
+);
+
+CREATE TABLE cities (
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(256) NOT NULL
+);
+
+CREATE TABLE test_places (
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    org_id integer,
+    city_id integer,
+    address varchar(1000),
+    position point NOT NULL,
+    price varchar(1000),
+    url varchar(1000),
+    is_urgent boolean,
+    CONSTRAINT fk_org_id
+        FOREIGN KEY(org_id)
+	    REFERENCES med_organisations(id),
+
+    CONSTRAINT fk_city_id
+        FOREIGN KEY(city_id)
+	    REFERENCES cities(id)
+);
