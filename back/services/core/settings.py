56becc4da53f0cd3c8bc10addc1444e62bec9518
Vova <bobturanov@gmail.com>
2020-12-13 02:22:59 +0300

    fix_dotenv

--- back/services/core/settings.py
+++ back/services/core/settings.py
@@ -1,9 +1,10 @@
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 
 
-load_dotenv(dotenv_path=f'{os.path.dirname(os.path.abspath(__file__))}/.env.prod')
+load_dotenv(dotenv_path=f'{Path(__file__).parents[2]}/.env.prod')
 
 DB_LOGIN = os.getenv("LOGIN")
 DB_PASSWORD = os.getenv("PASSWORD")
